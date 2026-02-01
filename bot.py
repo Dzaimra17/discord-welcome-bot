7f12ae309bf560296b9ab8edc80091f4a0cf069c
Dzaimra17 <dzimaraalmaliki@gmail.com>
2026-02-01 19:25:20 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,59 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
+# LOAD TOKEN DARI .env
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+# INTENTS
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-CHANNEL_ID = 1467477719766794260  # GANTI DENGAN ID CHANNEL WELCOME
-
+# BOT READY
 @bot.event
 async def on_ready():
-    print(f"Bot online sebagai {bot.user}")
+    print(f"Bot login sebagai {bot.user}")
 
+# MEMBER JOIN
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(CHANNEL_ID)
+    channel = discord.utils.get(member.guild.text_channels, name="welcome")
+    if channel is None:
+        return
 
     embed = discord.Embed(
         title="👋 Selamat Datang!",
-        description=f"Selamat datang {member.mention} di **{member.guild.name}**!",
+        description=f"Welcome {member.mention} ke **{member.guild.name}**!",
         color=discord.Color.green()
     )
     embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
-    embed.add_field(name="📌 Member ke-", value=f"{member.guild.member_count}", inline=True)
-    embed.set_footer(text="Selamat bergabung!")
+    embed.set_footer(text=f"Member ke-{member.guild.member_count}")
 
     await channel.send(embed=embed)
 
+# MEMBER LEAVE
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1467477719766794260)
+    channel = discord.utils.get(member.guild.text_channels, name="welcome")
+    if channel is None:
+        return
 
     embed = discord.Embed(
-        title="😢 Member Keluar",
-        description=f"**{member.name}** telah keluar dari server.",
+        title="😭 NOOOO",
+        description=f"**{member.name}** keluar dari server.",
         color=discord.Color.red()
     )
-    embed.set_footer(text="Semoga bertemu lagi!")
 
     await channel.send(embed=embed)
 
+# JALANKAN BOT (PALING BAWAH)
+bot.run(TOKEN)
+
+
 bot.run("MTQ2NzQ1MDQzMjg5NjEwNjU1OA.GfKtEZ.kXw3h5FoJ3Zu6rDlz7YWd0luy4z6mDSKtFTzq0")
+
